8a1095b0b251a0bd6162387bacbbf8ff1f77559b
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-27 22:20:57 +0300

    add Header component and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
 import Banner from './components/Banner';
-import Subheadings from './components/Subheadings';
-import TextSection from './components/TextSection';
-import Video from './components/Video';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <Subheadings />
-      <TextSection />
-      <Video />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+function Header() {
+  return <div className={styles.header}>Lorem Ipsum</div>;
+}
+
+export default Header;
