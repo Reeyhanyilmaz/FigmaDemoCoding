49a84d5697fb07e2c34d5c7dc3df98e2c55306aa
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-27 22:48:34 +0300

    add Carousel component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import Banner from './components/Banner';
+import Carousel from './components/Carousel';
 import Paragraph from './components/Paragraph';
+import Video from './components/Video';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Banner />
+      <Carousel />
       <Header />
       <Paragraph />
+      <Video />
     </div>
   );
 }
---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+function Carousel() {
+  return (
+    <div className={styles.carouselDiv}>
+        <img src="./assets/carousel/carousel.png" alt="carousel"/>      
+    </div>
+  )
+}
+
+export default Carousel;
