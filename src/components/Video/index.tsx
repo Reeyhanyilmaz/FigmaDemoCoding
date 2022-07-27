1428e2aba536b4bd84b98dc0c3af9d6ab2da5c4d
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-27 22:41:44 +0300

    add Video component and styles

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -4,22 +4,11 @@ import styles from "./styles.module.css";
 function Video() {
   return (
     <div>
-      {/* <span className={styles.videoContainer}> */}
         <iframe
             title="video"
-            className={styles.videoDiv}
-            src="./assets/video.png"
-            // frameBorder="0"
-            // allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            // allowFullScreen
+            className={styles.video}
+            src="./assets/video/video.png"
         />
-
-        {/* <span className={styles.playButton}>
-          <span className={styles.ellipse}>
-            <span className={styles.polygon}></span>
-          </span>
-        </span> */}
-      {/* </span> */}
     </div>
   );
 }
