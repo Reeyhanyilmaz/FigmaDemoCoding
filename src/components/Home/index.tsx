c549be5f87b05399bd9fc527fff1cf7a18ad7fd3
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-29 00:38:33 +0300

    change CSS

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -13,7 +13,7 @@ function Home() {
       <CarouselComp />
       <Subheadings />
       <Header />
-      <Paragraph />
+      {/* <Paragraph /> */}
       <Video />
       <CardsHeader />
       <Cards />
