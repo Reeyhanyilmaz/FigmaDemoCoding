9f999e22440e2880439f89333e6784d3279a2bc9
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-28 18:43:26 +0300

    change CSS

--- src/components/CardsHeader/index.js
+++ src/components/CardsHeader/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function CardsHeader() {
-  return <div className={styles.cardsHeader}> Suspendisse Tempus</div>;
+  return (
+    <div className={styles.cardsHeader}>
+      <div>Suspendisse Tempus</div>
+    </div>
+  );
 }
 
 export default CardsHeader;
