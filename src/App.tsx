823919fa7c0be9a5a0cbd6f3de4e4da84a26712d
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-28 20:04:15 +0300

    set router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import './App.css';
-import Carousel from './components/Carousel';
-import Subheadings from './components/Subheadings';
-import Paragraph from './components/Paragraph';
-import CardsHeader from './components/CardsHeader';
-import Video from './components/Video';
-import Cards from './components/Cards';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import ComingSoon from "./components/ComingSoon";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Carousel />
-      <Subheadings />
-      <Header />
-      <Paragraph />
-      <Video />
-      <CardsHeader/>
-      <Cards />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coming-soon" element={<ComingSoon />} />
+      </Routes>
     </div>
   );
 }
