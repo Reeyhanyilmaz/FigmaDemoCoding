e89b33f22cdd8eac1c2482a57fb235b4e52cfdfa
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-28 20:07:03 +0300

    add Link to and change style

--- src/components/Subheadings/index.tsx
+++ src/components/Subheadings/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './styles.module.css';
+import { Link , NavLink} from "react-router-dom";
 
 function Subheadings() {
+  // const [borderBottom , setBorderBottom] = useState(false);
+
+  // const handleClick = () => {
+  //   setBorderBottom(!borderBottom);
+  //   borderBottom ? styles.borderBottom  : styles.noneBottom 
+  // }
+
   return (
     <div className={styles.subheadingsDiv}>
+      <Link to="/coming-soon" className={styles.linkto}>
         <p className={styles.p1}>Lorem Ipsum</p>
+      </Link>
+      <Link to="/coming-soon" className={styles.linkto}>
         <p className={styles.p2}>Dolor Sit Ament</p>
-        <p className={styles.p3}>Consectetur adipiscing elit</p>       
-        <span className={styles.redLine}></span>
+      </Link>
+      <Link to="/coming-soon" className={styles.linkto}>
+        <p className={styles.p3}>Consectetur adipiscing elit</p>    
+       </Link>   
     </div>
   )
 }
