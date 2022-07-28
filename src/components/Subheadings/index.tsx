3545d898dae260c4bf2f85aedc3144f903aff0b2
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-29 00:38:11 +0300

    add chakra-ui Tabs

--- src/components/Subheadings/index.tsx
+++ src/components/Subheadings/index.tsx
@@ -1,28 +1,47 @@
-import React, {useState} from 'react';
-import styles from './styles.module.css';
-import { Link , NavLink} from "react-router-dom";
+import React from "react";
+import styles from "./styles.module.css";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Paragraph from "../Paragraph";
 
 function Subheadings() {
-  // const [borderBottom , setBorderBottom] = useState(false);
-
-  // const handleClick = () => {
-  //   setBorderBottom(!borderBottom);
-  //   borderBottom ? styles.borderBottom  : styles.noneBottom 
-  // }
-
   return (
     <div className={styles.subheadingsDiv}>
-      <Link to="/coming-soon" className={styles.linkto}>
+      <Tabs colorScheme="red" >
+        <TabList className={styles.tabList}>
+          <Tab color="#7F7F7F" className={styles.p}>Lorem Ipsum</Tab>
+          <Tab color="#7F7F7F" className={styles.p}>Dolor Sit Ament</Tab>
+          <Tab color="#7F7F7F" className={styles.p}>Consectetur adipiscing elit</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Paragraph />
+          </TabPanel>
+          <TabPanel>
+            <Paragraph />
+          </TabPanel>
+          <TabPanel>
+            <Paragraph />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      </div>
+
+
+  
+
+  );
+}
+
+export default Subheadings;
+
+
+{/* <Link to="/coming-soon" className={styles.linkto}>
         <span className={styles.p1}>Lorem Ipsum</span>
       </Link>
       <Link to="/coming-soon" className={styles.linkto}>
         <span className={styles.p2}>Dolor Sit Ament</span>
       </Link>
       <Link to="/coming-soon" className={styles.linkto}>
-        <span className={styles.p3}>Consectetur adipiscing elit</span>    
-       </Link>   
-    </div>
-  )
-}
-
-export default Subheadings;
+        <span className={styles.p3}>Consectetur adipiscing elit</span>
+      </Link> */}
