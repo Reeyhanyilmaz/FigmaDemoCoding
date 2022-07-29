624ac7e4f8f656b0b7b161475f3741a87f2790f9
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-29 10:33:04 +0300

    change styles with chakra-ui

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
+import { Text } from "@chakra-ui/react";
 
 function ComingSoon() {
   return (
     <div className={styles.coming}>
-      <em>
-        <h1>Coming Soon...</h1>
-      </em>
+      <Text
+        as="i"
+        color="white"
+        fontSize={{ base: "30px", md: "40px", lg: "100px" }}
+      >
+        Coming Soon...
+      </Text>
 
       <Link to="/" className={styles.linkto}>
-        <p>Back to Home</p>
+        <Text as="u">Back to Home</Text>
       </Link>
     </div>
   );
