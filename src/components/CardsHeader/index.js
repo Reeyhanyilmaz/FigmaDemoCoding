228618be6aef85640b7d7d4e188c2c6c26da2106
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-30 20:57:06 +0300

    change margin styles

--- src/components/CardsHeader/index.js
+++ src/components/CardsHeader/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function CardsHeader() {
-  return (
-    <div className={styles.cardsHeader}>
-      <div>Suspendisse Tempus</div>
-    </div>
-  );
+  return <div className={styles.cardsHeader}>Suspendisse Tempus</div>;
 }
 
 export default CardsHeader;
