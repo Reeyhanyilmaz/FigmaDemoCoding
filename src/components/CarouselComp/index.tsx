34f444e09a540b52cdd012a23acde5bec7ffe57b
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-30 19:10:07 +0300

    add indicators and styles

--- src/components/CarouselComp/index.tsx
+++ src/components/CarouselComp/index.tsx
@@ -13,7 +13,6 @@ function CarouselComp() {
         autoPlay={true}
         interval={5000}
         transitionTime={1000}
-        
         //next, prev
         renderArrowNext={(clickHandler) => (
           <div className={styles.arrowNext} onClick={clickHandler}></div>
@@ -21,7 +20,16 @@ function CarouselComp() {
         renderArrowPrev={(clickHandler) => (
           <div className={styles.arrowPrev} onClick={clickHandler}></div>
         )}
-      >
+        //indicators
+        renderIndicator={(clickHandler, isSelected) => {
+          return (
+              <span
+                className={styles.indicator}
+                onClick={clickHandler}
+                style={{ border: `2px solid ${isSelected ? "white" : "rgba(225,225,225,0.5)"}`}}/>
+          );
+        }}
+        >
         <div>
           <img src="./assets/carousel/carousel.png" alt="carousel" />
         </div>
@@ -35,10 +43,9 @@ function CarouselComp() {
           <img src="./assets/carousel/carousel.png" alt="carousel" />
         </div>
       </Carousel>
- 
-     {/* pagination line */}
-      <div className={styles.paginationLine}></div>
 
+      {/* pagination line */}
+      <div className={styles.paginationLine}></div>
     </div>
   );
 }
